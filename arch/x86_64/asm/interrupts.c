8e689d128f9a564b7dd97cd74d75c95de6d0a9a1
km5762 <kumyles@gmail.com>
2026-09-25 20:57:24 -0400

    fixing interrupts

--- arch/x86_64/asm/interrupts.c
+++ arch/x86_64/asm/interrupts.c
@@ -1,14 +1,19 @@
 #include "interrupts.h"
-
+#include <stdbool.h>
 #include <stdint.h>
 
 struct idt_entry {
   uint16_t isr_low;   // The lower 16 bits of the ISR's address
   uint16_t kernel_cs; // The GDT segment selector that the CPU will load into CS
                       // before calling the ISR
-  uint8_t reserved;   // Set to zero
+  uint8_t ist; // The IST in the TSS that the CPU will load into RSP; set to
+               // zero for now
   uint8_t attributes; // Type and attributes; see the IDT page
-  uint16_t isr_high;  // The higher 16 bits of the ISR's address
+
+  uint16_t
+      isr_mid; // The higher 16 bits of the lower 32 bits of the ISR's address
+  uint32_t isr_high; // The higher 32 bits of the ISR's address
+  uint32_t reserved; // Set to zero
 } __attribute__((packed));
 
 #define IDT_MAX_DESCRIPTORS 256
@@ -16,7 +21,7 @@ __attribute__((aligned(0x10))) static struct idt_entry idt[256];
 
 struct idtr {
   uint16_t limit;
-  uint32_t base;
+  uint64_t base;
 } __attribute__((packed));
 
 static struct idtr idtr;
@@ -28,11 +33,13 @@ void exception_handler() {
 void interrupts_set_descriptor(uint8_t vector, void *isr, uint8_t flags) {
   struct idt_entry *descriptor = &idt[vector];
 
-  descriptor->isr_low = (uint32_t)isr & 0xFFFF;
-  descriptor->kernel_cs = 0x08; // this value can be whatever offset your kernel
-                                // code selector is in your GDT
+  descriptor->isr_low = (uint64_t)isr & 0xFFFF;
+  descriptor->kernel_cs = 0x8;
+  descriptor->ist = 0;
   descriptor->attributes = flags;
-  descriptor->isr_high = (uint32_t)isr >> 16;
+
+  descriptor->isr_mid = ((uint64_t)isr >> 16) & 0xFFFF;
+  descriptor->isr_high = ((uint64_t)isr >> 32) & 0xFFFFFFFF;
   descriptor->reserved = 0;
 }
 
--- src/kernel.c
+++ src/kernel.c
@@ -68,12 +68,12 @@ void kernel_main(unsigned long magic, unsigned long multiboot_address) {
   parse_multiboot(HIGHER_HALF_ADDRESS(multiboot_address));
   graphics_set_screen(&kernel.graphics, 0x1e1e2e);
   terminal_print(&kernel.terminal, "HELLO\n");
-  // interrupts_init();
-  // interrupts_set_descriptor(0, zero_handler,
-  //                           INTERRUPT_DESCRIPTOR_FLAGS_TRAP_GATE);
-  // volatile int a = 1;
-  // volatile int b = 0;
-  // volatile int c = a / b;
-  //
-  // (void)c;
+  interrupts_init();
+  interrupts_set_descriptor(0, zero_handler,
+                            INTERRUPT_DESCRIPTOR_FLAGS_TRAP_GATE);
+  volatile int a = 1;
+  volatile int b = 0;
+  volatile int c = a / b;
+
+  (void)c;
 }
